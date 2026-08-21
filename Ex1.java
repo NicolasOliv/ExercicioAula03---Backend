b1ec4e9b25eb0425ba26cf2767ec7e266435b010
Nicolas Oliveira <oliveira.nicolasmiguel@gmail.com>
2026-08-21 19:56:58 -0300

    first commit

---
+++ Ex1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "!");
+
+        scanner.close();
+    }
+}
---
+++ Ex6.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu peso (kg): ");
+        double peso = scanner.nextDouble();
+
+        System.out.print("Digite sua altura (m): ");
+        double altura = scanner.nextDouble();
+
+        double imc = peso / (altura * altura);
+
+        System.out.println("Seu IMC é: " + imc);
+
+        if (imc < 18.5) {
+            System.out.println("Classificação: Abaixo do peso");
+        } else if (imc < 25) {
+            System.out.println("Classificação: Peso normal");
+        } else if (imc < 30) {
+            System.out.println("Classificação: Sobrepeso");
+        } else {
+            System.out.println("Classificação: Obesidade");
+        }
+
+        scanner.close();
+    }
+}
